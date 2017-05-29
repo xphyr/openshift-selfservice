dcf5452dab3e0602452eff8199052ea942663876
u220374 <reto.lehmann@sbb.ch>
2017-05-29 07:48:41 +0200

    Removed angular

--- ui/src/app/core/module-import-guard.ts
+++
@@ -1,5 +0,0 @@
-export function throwIfAlreadyLoaded(parentModule: any, moduleName: string) {
-   if(parentModule){
-       throw new Error(`${moduleName} has already been loaded. Import core modules in the AppModule only.`);
-   }
-}
